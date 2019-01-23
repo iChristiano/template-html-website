d26a3acbb2a4d585143f780e772a1bd94dbe3b07
iChristiano <christiano.dutti@gmail.com>
2019-01-23 19:02:04 +0100

    Add first draft

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+﻿/* Navigation */
+function openNav() {
+  document.getElementById('mobileNav').style.width = '250px';
+
+
+  document.getElementById('closebtn').style.display = 'flex';
+  document.getElementById('openbtn').style.display = 'none';
+
+  document.getElementById('modalNav').style.display = 'block';
+}
+
+function closeNav() {
+  document.getElementById('mobileNav').style.width = '0';
+
+  document.getElementById('openbtn').style.display = 'flex';
+  document.getElementById('closebtn').style.display = 'none';
+
+  document.getElementById('modalNav').style.display = 'none';
+}
+
+
+/* Scroll */
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementsByClassName("main-nav")[0].style.justifContent= "flex-start";
+    document.getElementsByClassName("main-nav")[0].style.fontWeight= "normal";
+    document.getElementsByClassName("main-nav")[0].lastElementChild.classList.add('push-left');
+
+  } else {
+    document.getElementsByClassName("main-nav")[0].style.justifContent= "center";
+    document.getElementsByClassName("main-nav")[0].style.fontWeight= "bold";
+    document.getElementsByClassName("main-nav")[0].lastElementChild.classList.remove('push-left');
+  }
+}
+window.onscroll = function() {scrollFunction()};
---
+++ readme.md
@@ -0,0 +1,21 @@
+﻿# Readme example
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, accusantium sunt earum rem animi laudantium possimus hic temporibus non nobis corporis neque harum exercitationem blanditiis rerum nesciunt eius dolores ab?
+
+---
+
+## Some markdown
+
+link: [link](https://www.google.com)
+
+italics: *asterisks*.
+
+bold: **asterisks**
+
+lists: 
+* first entry
+* second entry
+
+ 1. first entry
+ 2. second entry
+
+> Blockquote
